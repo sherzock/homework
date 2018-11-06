f2565289ef9a70563f132dd68eb9b6c8bb9f9ca9
sherzock <jacobo.galofre@gmail.com>
2018-11-06 12:28:45 +0100

    todos9

---
+++ Dev_class9_handout/Motor2D/j1PerfTimer.cpp
@@ -0,0 +1,40 @@
+// ----------------------------------------------------
+// j1PerfTimer.cpp
+// Slow timer with microsecond precision
+// ----------------------------------------------------
+
+#include "j1PerfTimer.h"
+#include "SDL\include\SDL_timer.h"
+
+uint64 j1PerfTimer::frequency = 0;
+
+// ---------------------------------------------
+j1PerfTimer::j1PerfTimer()
+{
+	// TODO 2: Fill Constructor, Start(),ReadMs() and ReadTicks() methods
+	// they are simple, one line each!
+
+	Start();
+}
+
+// ---------------------------------------------
+void j1PerfTimer::Start()
+{
+	started_at = ReadMs();
+	frequency = ReadTicks();
+}
+
+// ---------------------------------------------
+double j1PerfTimer::ReadMs() const
+{
+	return SDL_GetPerformanceCounter();
+}
+
+// ---------------------------------------------
+uint64 j1PerfTimer::ReadTicks() const
+{
+	SDL_GetPerformanceCounter() - started_at / SDL_GetPerformanceFrequency();
+	return 0;
+}
+
+
